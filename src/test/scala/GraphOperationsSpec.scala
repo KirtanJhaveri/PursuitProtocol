fcc804cf24d801f70dd86269d2cf36875adc407e
kirtanjhaveri2000@gmail.com <kirtanjhaveri2000@gmail.com>
2023-11-21 22:47:23 -0600

    fixed a unit test

--- src/test/scala/GraphOperationsSpec.scala
+++ src/test/scala/GraphOperationsSpec.scala
@@ -26,8 +26,8 @@ class GraphOperationsSpec extends AnyFlatSpec with Matchers with BeforeAndAfterA
 
   "processGraph" should "return the correct successor node ID for a given node" in {
     val graph = GraphOperations.createGraph(nodes, edges)
-    val emptyGraph = GraphOperations.createGraph(List.empty[NodeObject], List.empty[Action])
-    val successorNodeId = GraphOperations.processGraph(nodes, 1, graph,emptyGraph)
+    val graph2 = GraphOperations.createGraph(nodes, edges)
+    val successorNodeId = GraphOperations.processGraph(nodes, 1, graph,graph2)
     successorNodeId shouldBe 2
   }
 }
